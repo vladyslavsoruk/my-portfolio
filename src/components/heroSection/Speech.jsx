174ad9ab1b62d53f878118ee34a68769f880a9ed
vladyslavsoruk <sorukv@gmail.com>
2025-04-17 22:30:13 +0300

    text in heroSection changed, svgs added

--- src/components/heroSection/Speech.jsx
+++ src/components/heroSection/Speech.jsx
@@ -6,10 +6,10 @@ function Speech() {
     <motion.div
       className="bubbleContainer"
       animate={{ opacity: [0, 1] }}
-      transition={{ duration: 1 }}
+      transition={{ duration: 1.5 }}
     >
       <div className="bubble">
-        <TypeAnimation
+        {/* <TypeAnimation
           sequence={[
             1000,
             "Same substring at the start will only be typed out once, initially",
@@ -21,7 +21,12 @@ function Speech() {
           speed={40}
           deletionSpeed={60}
           repeat={Infinity}
-        />
+        /> */}
+        <p className="bubble-text">
+          I`m a 22-year-old Web Developer. I`ve been programming for{" "}
+          <span>3</span> years. I like creating complex web applications and
+          continuously learning new stuff.
+        </p>
       </div>
       <img src="/man.png" alt="" />
     </motion.div>
